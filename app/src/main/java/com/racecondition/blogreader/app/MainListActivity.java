3a23d25c11d20dd31edd66dee0c1fe07b4925dce
TimB0 <tboland1@gmail.com>
2014-04-06 00:38:04 -0700

    added @string Resources

--- app/src/main/java/com/racecondition/blogreader/app/MainListActivity.java
+++ app/src/main/java/com/racecondition/blogreader/app/MainListActivity.java
@@ -1,56 +1,50 @@
 package com.racecondition.blogreader.app;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames = {
-            "Android beta",
-            "Android 1.0",
-            "Android 1.1",
-            "Cupcake",
-            "Donut",
-            "Eclair",
-            "Froyo",
-            "Gingerbread",
-            "Honeycomb",
-            "Ice Cream Sandwich",
-            "Jelly Bean"
-    };
+    protected String[] mAndroidNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
+        Resources resources = getResources();
+        mAndroidNames = resources.getStringArray(R.array.android_names);
+
         ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
         setListAdapter(adapter);
+
+        //Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_list, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }*
+//
+//}
