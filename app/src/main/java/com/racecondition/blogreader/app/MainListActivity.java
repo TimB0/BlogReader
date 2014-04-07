46ebe828e47c217a99878940c55f0afa512ae579
TimB0 <tboland1@gmail.com>
2014-04-06 17:40:34 -0700

    Parsing Data Returned in JSON Format

--- app/src/main/java/com/racecondition/blogreader/app/MainListActivity.java
+++ app/src/main/java/com/racecondition/blogreader/app/MainListActivity.java
@@ -10,6 +10,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -78,7 +81,19 @@ public class MainListActivity extends ListActivity {
                         char[] charArray = new char[contentLength];
                         reader.read(charArray);
                         String responseData = new String(charArray);
-                        Log.v(TAG, responseData);
+
+                        JSONObject jsonResponse = new JSONObject(responseData);
+                        String status = jsonResponse.getString("status");
+                        Log.v(TAG, status);
+
+                        JSONArray jsonPosts = jsonResponse.getJSONArray("posts");
+                        for (int i = 0; i < jsonPosts.length(); i++) {
+                            JSONObject jsonPost = jsonPosts.getJSONObject(i);
+                            String title = jsonPost.getString("title");
+                            Log.v(TAG, "Post " + i +": " + title);
+                        }
+
+
                     }
                     else {
                         Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
